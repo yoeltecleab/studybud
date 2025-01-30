00711ce2a38806e22e444f2062d8b8057c00fb25
Yoel Tecleab <ytecleab@charlotte.edu>
2025-01-30 00:56:46 -0500

    Timestamp: 1:04:25 - Database & Admin Panel

--- base/urls.py
+++ base/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from . import views
 
+from . import views
 
 urlpatterns = [
     path('', views.home, name="home"),
-    path('room/', views.room, name="room"),
+    path('room/<str:pk>/', views.room, name="room"),
 ]
