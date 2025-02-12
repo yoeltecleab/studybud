fcb6b4e970150dd054e8ebf2392e21dc58550f55
Yoel Tecleab <ytecleab@charlotte.edu>
2025-02-12 00:57:40 -0500

    Timestamp: 2:39:03- User Logout

--- base/views.py
+++ base/views.py
@@ -1,3 +1,6 @@
+from django.contrib import messages
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.models import User
 from django.db.models import Q
 from django.shortcuts import render, redirect
 
@@ -15,6 +18,23 @@ from .models import Room, Topic
 # ]
 
 def loginPage(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        try:
+            user = User.objects.get(username=username)
+        except:
+            messages.error(request, 'User does not exist')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'Username OR password does not exist')
+
     context = {}
     return render(request, 'base/login_register.html', context)
 
