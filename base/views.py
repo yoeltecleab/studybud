759d94d288765eec7ecb2958e9f45767dc513190
Yoel Tecleab <ytecleab@charlotte.edu>
2025-02-11 23:48:04 -0500

    Timestamp: 2:32:25 - Flash Messages

--- base/views.py
+++ base/views.py
@@ -14,6 +14,10 @@ from .models import Room, Topic
 #     {'id': 3, 'name': 'Frontend Developers'},
 # ]
 
+def loginPage(request):
+    context = {}
+    return render(request, 'base/login_register.html', context)
+
 
 def home(request):
     q = request.GET.get('q') if request.GET.get('q') is not None else ''
@@ -22,6 +26,7 @@ def home(request):
         Q(topic__name__icontains=q) |
         Q(description__icontains=q) |
         Q(name__icontains=q))
+
     topics = Topic.objects.all()
     room_count = rooms.count()
     context = {'rooms': rooms,
