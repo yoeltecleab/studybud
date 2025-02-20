cb921dd5915bb09c8a4c393b8ed3184191a1e373
Yoel Tecleab <ytecleab@charlotte.edu>
2025-02-20 13:13:24 -0500

    Timestamp: 5:35:02 - Mobile Responsiveness

--- base/views.py
+++ base/views.py
@@ -7,7 +7,7 @@ from django.db.models import Q
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
 
-from .forms import RoomForm
+from .forms import RoomForm, UserForm
 from .models import Room, Topic, Message
 
 
@@ -151,7 +151,7 @@ def updateRoom(request, pk):
     if request.method == 'POST':
         topic_name = request.POST.get('topic')
         topic, created = Topic.objects.get_or_create(name=topic_name)
-        
+
         room.name = request.POST.get('name')
         room.topic = topic
         room.description = request.POST.get('description')
@@ -188,3 +188,17 @@ def deleteMessage(request, pk):
         message.delete()
         return redirect('home')
     return render(request, 'base/delete.html', {'obj': message})
+
+
+@login_required(login_url='login')
+def updateUser(request):
+    user = request.user
+    form = UserForm(instance=user)
+
+    if request.method == 'POST':
+        form = UserForm(request.POST, instance=user)
+        if form.is_valid():
+            form.save()
+            return redirect('user-profile', pk=user.id)
+
+    return render(request, 'base/update-user.html', {'form': form})
